a6d9b3003b276c4ea5d82e024aa2574496ffb918
Fenbih22 <bihfenmuma@gmail.com>
2022-09-22 23:56:31 +0000

    version2

--- deploy.py
+++ deploy.py
@@ -1,4 +1,5 @@
 # Deploying a python web application
-echo "Hello World"   = shell scripts 
+echo "Hello World"
 #= shell scripts   
-print('Hello World') = python script  
+print('Hello World')
+#= python script  
